82a87c9dc1fca68750c805da229b5109f64ec945
SalvadorDuarte <salvador.sdp@gmail.com>
2015-08-19 17:11:53 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 //Definición de rutas de session
 router.get('/login', sessionController.new); //formulario login
@@ -33,6 +34,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 //Definición de rutas de autor
 router.get('/quizes/author', quizController.author);
