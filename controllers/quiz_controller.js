36c29f277836678b9bdcbc7e7120cc1622e9f086
SalvadorDuarte <salvador.sdp@gmail.com>
2015-08-19 13:24:10 +0200

    Gestión de Listas de Quizes: Creación, Edición y Borrado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 //Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 		function(quiz) {
 			if (quiz) {
 				req.quiz=quiz;
@@ -29,7 +29,7 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});	
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //OLD: Método antiguo de una pagina con pregunta estatica
@@ -55,15 +55,85 @@ exports.answer = function(req, res) {
 		resultado = 'Correcto';
 	}
 	
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});	
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});	
 };
 
 //GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});	
-	})
+	models.Quiz.findAll().then(
+		function(quizes){
+			res.render('quizes/index', {quizes: quizes, errors: []});	
+		}
+	).catch(function(error){next(error)});
 };
+
+//GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+		// crea objeto quiz
+			{pregunta:"Pregunta", respuesta:"Respuesta"}
+		);
+
+		res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+//GET /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	quiz.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				// guarda en DB los campos pregunta y respuesta de quiz
+				quiz.save({fields: ["pregunta", "respuesta"]}).then(
+					function(){
+						res.redirect('/quizes'); // Redirección HTTP (URL relativo) a lista de preguntas
+					});
+			}
+		}
+	);
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/edit', {quiz: quiz, errors: err.errors});
+			} else {
+				// guarda en DB los campos pregunta y respuesta de quiz
+				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(
+					function(){
+						res.redirect('/quizes'); // Redirección HTTP (URL relativo) a lista de preguntas
+					});
+			}
+		}
+	);
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then(
+		function(){
+			res.redirect('/quizes');
+		}
+	).catch(function(error){next(error)});
+};
+
 
 //GET /author
 exports.author = function(req, res) {
